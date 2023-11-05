e3ae5d439ffeafa55f47fe28677f6690e1a9a2cd
ananyajain10 <jainananya355@gmail.com>
2023-11-05 21:11:21 +0530

    dbSetup

--- app.js
+++ app.js
@@ -1,14 +1,70 @@
-const http = require('node:http');
+const express = require("express");
+require('dotenv').config();
 
-const hostname = '127.0.0.1';
 const port = 3000;
+const bodyParser = require("body-parser");
+const mongoose = require('mongoose');
+const ChannelModel = require('./models/channel');
+const dbUrl = process.env.url;
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello, World!\n');
+//mongoose
+const connectionParams = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}
+
+mongoose
+    .connect(dbUrl, connectionParams)
+    .then(()=>{
+        console.info("connected")
+    })
+     .catch((e) => {
+        console.log("error:",e);
+     });
+    
+var path = require('path');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get("/home",function(req, res){
+    console.log("get");
+    res.send("hello world");
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-}); 
+
+
+app.post("/home", (req, res) => {
+   
+    const name = req.body.name;
+    const email = req.body.email;
+    console.log("post");
+    res.send("saved");
+
+
+const channelModel = new ChannelModel();
+
+  channelModel instanceof ChannelModel;
+  channelModel instanceof mongoose.Model;
+  channelModel instanceof mongoose.Document;
+
+  channelModel.name = name;
+  channelModel.email = email;
+
+  var output;
+  (async () => {
+    output = await channelModel.save();
+
+  });
+
+  channelModel.save().then(savedDoc => {
+    savedDoc === channelModel;
+    console.log(savedDoc);
+  });
+});
+
+app.listen(port, () => {
+    console.log(`server listening at ${port}`);
+});
